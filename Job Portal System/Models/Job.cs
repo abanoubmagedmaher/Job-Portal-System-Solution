9f6775b62c1b6952e78846af236ede015a6581d1
abanoub maged <abanoubmagedmaher@gmail.com>
2024-11-29 21:47:14 +0200

    up

---
+++ Job Portal System/Models/Job.cs
@@ -0,0 +1,12 @@
+﻿namespace Job_Portal_System.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public string Requirements { get; set; }
+    }
+}
